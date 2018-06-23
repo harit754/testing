e7d8fa9309a792035a9ae1831b406c54faa2e431
harit754 <haritsaini754@gmail.com>
2018-06-24 04:09:58 +0530

    expect package

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,41 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
 
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}.`)
-  }
+  expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
   var res = utils.square(3);
 
-  if (res !== 9) {
-    throw new Error(`Expected 9, but got ${res}.`);
-  }
+  expect(res).toBe(9).toBeA('number');
 });
+
+// should verify first and last names are set
+// assert it includes firstName and lastName with proper values
+it('should set firstName and lastName', () => {
+  var user = {location: 'Philadelphia', age: 25};
+  // var res = utils.setName(user, 'Harit Saini');
+  var res=utils.setName(user,`Harit Saini`);
+
+  expect(res).toInclude({
+    firstName: 'Harit',
+    lastName: 'Saini'
+  });
+});
+
+// it('should expect some values', () => {
+//   expect(12).toBe(12);
+//   expect({name: 'andrew'}).toNotEqual({name: 'Andrew'});
+//   expect([2,3,4]).toExclude(152);
+//   expect({
+//     name: 'Andrew',
+//     age: 25,
+//     location: 'Philadelphia'
+//   }).toExclude({
+//     age: 23
+//   })
+// });
